2aa05276ba3efcf59abfebd79abedf976bf09559
Krystian Kedron <kedronkrystian@gmail.com>
2019-09-14 18:59:35 +0200

    Update navbar component:  - update style  - new design

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable="lg" type="dark" class="orlen-color">
       <b-navbar-brand href="#">Qubit</b-navbar-brand>
+      <!--  On the middle  -->
+      <b-navbar-brand class="brand-center"> CONFIGURATION CHECKER </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -13,20 +15,8 @@
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
+        <b-navbar-nav class="ml-auto center-768">
+          <img src="../assets/orlen-logo.png">
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -40,5 +30,29 @@
 </script>
 
 <style scoped>
+  .orlen-color {
+    background-color: #5a5a5a !important;
+  }
+  img {
+    width: 5vw;
+  }
+  .brand-center {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  @media only screen and (max-width: 980px) {
+
+    .brand-center {
+      position: relative !important;
+      left: auto;
+      transform: translateX(0%);
+    }
+
+    .center-768 {
+      display: block !important;
+    }
+  }
 
 </style>
